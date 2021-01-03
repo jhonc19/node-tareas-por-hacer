e1ff8893512592587a065dd4a572f2eab2682e49
Jhon Choque <jhonc.choquec@gmail.com>
2021-01-02 19:05:35 -0500

    Primer commit

---
+++ config/yargs.js
@@ -0,0 +1,29 @@
+const descripcion = {
+    alias: 'd',
+    desc: 'Descripción de la tarea por hacer',
+    demand: true
+};
+
+const completado = {
+    alias: 'c',
+    default: true,
+    desc: 'Marca como completado o pendiente la tarea'
+};
+
+const argv = require('yargs')
+    .command('crear', 'Crear un elemento por hacer', {
+        descripcion
+    })
+    .command('actualizar', 'Actualiza el estado completado de una tarea', {
+        descripcion,
+        completado
+    })
+    .command('borrar', 'Borra un elemento por hacer', {
+        descripcion
+    })    
+    .help()
+    .argv;
+
+module.exports = {
+    argv
+}    
